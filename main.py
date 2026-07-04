222920c23c965d0f03da903ea3c20726b75edfdc
Sourabh Tomar <sourabhtomar6709@gmail.com>
2026-07-04 13:45:19 +0530

    feat(V2): connected static assets and HTML templates to the core application routes

--- main.py
+++ main.py
@@ -43,11 +43,27 @@ def get_posts():
 
 from fastapi.responses import HTMLResponse
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False) # By doing include_in_schema=False we are saying dont include these in fastapi docs as these are html endpoints which are only used for humans to see and not used for programmable logic but this written logic here, this code will still work in the browser. 
-@app.get("/posts", response_class=HTMLResponse ,include_in_schema=False) # We can stack multiple decorators on the same function
-def home():
-    return f"<h1>{posts[0]["title"]}</h1>"
+# @app.get("/", response_class=HTMLResponse, include_in_schema=False) # By doing include_in_schema=False we are saying dont include these in fastapi docs as these are html endpoints which are only used for humans to see and not used for programmable logic but this written logic here, this code will still work in the browser. 
+# @app.get("/posts", response_class=HTMLResponse ,include_in_schema=False) # We can stack multiple decorators on the same function
+# def home():
+#     return f"<h1>{posts[0]["title"]}</h1>"
 
 
 
+# Video - 2
 
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/static", StaticFiles(directory="static"), name="static") # This mount method takes 3 arguments :- 1. the url path where the static files will be accessible, 2. static file instance that points to our static directory, 3. name we can use to reference in our templates  
+
+
+templates = Jinja2Templates(directory="templates") # We created a template object that knows where our templates directory is 
+
+# After writing the above step i commented out the previous routes that we wrote under video - 1
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request): # We will be adding a request parameter as a argument here because it is required by jinja2 (template engine)
+    return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"}, ) # The 3rd argument here is referred as a context dictionary adn this dictionary contains all the variables that we wanna be able to use in our template. The first variable we passed in is 
